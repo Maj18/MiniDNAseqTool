cea6553655ddccf69aeb39d773672498ab57c4a0
YUAN LI <45690154+Maj18@users.noreply.github.com>
2021-03-07 21:11:53 +0100

    Update __init__.py

--- src/miniseqtoolbox/__init__.py
+++ src/miniseqtoolbox/__init__.py
@@ -1,6 +1,5 @@
-
 from .my_bio_module import getGCPercntage
 from .my_bio_module import rev_complement
 from .my_bio_module import oligo_match
 from .my_bio_module import oligo_match_file
-__version__"1.1.3"
+
